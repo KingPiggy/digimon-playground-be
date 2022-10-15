418faf8c44b419ed0618e62757c263c7dd233d29
HoonDragonite <lsoonnoon@gmail.com>
2022-10-15 15:05:22 +0900

    add : cors 해결을 위한 web config

---
+++ api-server/src/main/java/com/kingpiggy/digimon/pg/configuration/WebConfig.java
@@ -0,0 +1,32 @@
+package com.kingpiggy.digimon.pg.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * WebConfig class.
+ * <PRE>
+ * Describe here.
+ * </PRE>
+ *
+ * <PRE>
+ * <B>History:</B>
+ * damian.lee, 0.1.0, Created at 2022.10.15
+ * </PRE>
+ *
+ * @author : SEUNGHOON
+ * @version : 0.1.0
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .maxAge(3000);
+    }
+
+}
